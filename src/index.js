d46ec02fa2df352603ee485f978b7596c449a252
Dima <directordmh@tut.by>
2020-03-13 10:10:56 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2) return false
+    let v = str, c
+    for(let i = 0; i < str.length; i++) {
+        c = v.replace(/(\[\])|(\(\))|(\{\})|(\|\|)|(12)|(34)|(56)|(77)|(88)/, '')
+        if (v === c) if (c.length > 0) return false
+        else return true
+        v = c
+    }
 }
+
